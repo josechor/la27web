09613099706a128d0bc63debc4f7d8e7e04e1d38
Jose Ramon <jrpaz@boardfy.com>
2024-12-16 18:00:31 +0100

    User Profile

--- src/shared/services/tuips/tuipsFetchApi.ts
+++ src/shared/services/tuips/tuipsFetchApi.ts
@@ -4,7 +4,7 @@ import { TuipsApi } from "./tuipsApi";
 
 export class TuipsFetchApi implements TuipsApi {
   async getTuips(page: number, limit: number): Promise<Tuip[]> {
-    return await apiGet<any>(`/api/tuips?page=${page}&limit=${limit}`);
+    return await apiGet<Tuip[]>(`/api/tuips?page=${page}&limit=${limit}`);
   }
   async createTuip(tuip: TuipCreate): Promise<void> {
     await apiPost("/api/tuips", tuip);
--- src/user/page/UserProfile.vue
+++ src/user/page/UserProfile.vue
@@ -3,7 +3,14 @@ import { onMounted, ref, Ref } from "vue";
 import { useRoute } from "vue-router";
 import { useUserStore } from "../../shared/stores/user/userStore";
 import { User } from "../../shared/types/userTypes";
+import Button from "../../shared/atoms/buttons/Button.vue";
+import { ButtonSize } from "../../shared/types/shared";
+import { computed } from "@vue/reactivity";
+import { TuipsFetchApi } from "../../shared/services/tuips/tuipsFetchApi";
+import Tuip from "../../shared/components/Tuip.vue";
+import { Tuip as TuipInterface } from "../../shared/types/tuipsTypes";
 
+const tuipsFetchApi = new TuipsFetchApi();
 const userStore = useUserStore();
 
 const route = useRoute();
@@ -11,14 +18,115 @@ const username = route.params.username;
 
 const user: Ref<User | null> = ref(null);
 
+const tuips: Ref<TuipInterface[]> = ref([]);
+const page = ref(1);
+const limit = 20;
 onMounted(async () => {
   if (!username || typeof username !== "string") {
     console.error("El parámetro userId no es válido");
     return;
   }
-  user.value = (await userStore.getUserData(username)) || null;
+
+  Promise.all([
+    userStore.getUserData(username),
+    tuipsFetchApi.getTuips(page.value, limit),
+  ]).then(([userData, tuipsData]) => {
+    user.value = userData || null;
+    tuips.value = tuipsData;
+  });
+});
+
+function handleClickFollow() {
+  console.log("Follow");
+}
+
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+const getUserBirthday = computed(() => {
+  console.log(user.value?.birthday);
+  if (!user.value?.birthday) return "";
+  const date = new Date(user.value.birthday);
+  return `${date.getDate()} de ${
+    months[date.getMonth()]
+  } de${date.getFullYear()}`;
+});
+
+const getUserCreatedAt = computed(() => {
+  if (!user.value?.createdAt) return "";
+  const date = new Date(user.value.createdAt);
+  return `Cuenta creada el ${date.getDate()} de ${
+    months[date.getMonth()]
+  } de ${date.getFullYear()}`;
 });
 </script>
 <template>
-  {{ user }}
+  <div class="flex gap-6" v-if="user">
+    <section
+      class="border border-y-0 border-x-light-background-colors-quaternary dark:border-x-dark-background-color-quaternary w-[65%] min-h-screen"
+    >
+      <header
+        class="border border-x-0 border-t-0 border-b-light-background-colors-quaternary dark:border-b-dark-background-color-quaternary pb-6"
+      >
+        <div class="relative">
+          <img
+            class="w-full h-[200px] object-cover"
+            src="https://www.excelsior.com.mx/media/styles/image1200x630/public/pictures/2024/11/21/3216015.jpg"
+            alt=""
+          />
+          <img
+            class="h-[80px] w-[80px] rounded-full absolute -bottom-[40px] left-0"
+            src="https://media.istockphoto.com/id/1130884625/vector/user-member-vector-icon-for-ui-user-interface-or-profile-face-avatar-app-in-circle-design.jpg?s=612x612&w=0&k=20&c=1ky-gNHiS2iyLsUPQkxAtPBWH1BZt0PKBB1WBtxQJRE="
+            alt="user"
+          />
+        </div>
+        <div class="flex justify-end items-center p-6">
+          <Button
+            @click="handleClickFollow"
+            :size="ButtonSize.large"
+            text="Seguir"
+            class=""
+          />
+        </div>
+        <div class="flex flex-col gap-4 px-6">
+          <div class="flex flex-col gap-0.5">
+            <span class="font-bold text-lg">{{ user.demonName }}</span>
+            <span class="font-light text-sm">@{{ user.userName }}</span>
+          </div>
+          <span v-if="user.description">{{ user.description }}</span>
+          <div>
+            {{ user.birthday }}
+            <span v-if="user.birthday">{{ getUserBirthday }}</span>
+            <span v-if="user.createdAt">{{ getUserCreatedAt }}</span>
+          </div>
+          <div class="flex flex-row gap-4 text-sm">
+            <div class="flex gap-[2px]">
+              <span class="font-extrabold">{{ user.following }}</span
+              ><span>Siguiendo</span>
+            </div>
+            <div class="flex gap-[2px]">
+              <span class="font-extrabold">{{ user.followers }}</span
+              ><span>seguidores</span>
+            </div>
+          </div>
+        </div>
+      </header>
+      <section class="p-6">
+        <Tuip v-for="tuip in tuips" :key="tuip.tuipId" :tuip="tuip" />
+      </section>
+    </section>
+    <section class="w-[35%]"></section>
+  </div>
 </template>
