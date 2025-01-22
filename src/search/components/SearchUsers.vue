7b9a09e5ed655964e45604060d9175792d2b151a
Jose Ramon <jrpaz@boardfy.com>
2025-01-22 13:40:25 +0100

    wip

--- src/search/components/SearchUsers.vue
+++ src/search/components/SearchUsers.vue
@@ -35,42 +35,46 @@ async function handleClickFollow() {
 <template>
   <div
     @click="$router.push(`/profile/${userModel.userName}`)"
-    class="p-2 flex flex-row justify-between items-start gap-5 cursor-pointer"
+    class="p-2 flex flex-col justify-between items-start gap-5 cursor-pointer"
   >
-    <div class="flex flex-row gap-3">
-      <div class="w-10 h-10 min-w-10 min-h-10">
-        <Image
-          :src="userModel.profilePicture"
-          class="rounded-full w-full h-full object-cover"
+    <div
+      class="p-2 flex flex-row justify-between items-start gap-5 cursor-pointer w-full"
+    >
+      <div class="flex flex-row gap-3">
+        <div class="w-10 h-10 min-w-10 min-h-10">
+          <Image
+            :src="userModel.profilePicture"
+            class="rounded-full w-full h-full object-cover"
+          />
+        </div>
+        <div class="flex flex-col gap-1">
+          <div class="flex flex-col">
+            <span class="font-bold">{{ userModel.demonName }}</span>
+            <span class="text-text-color-secondary font-extralight"
+              >@{{ userModel.userName }}</span
+            >
+          </div>
+        </div>
+      </div>
+      <div class="lg:mr-6 mt-1 hidden lg:block">
+        <Button
+          v-if="loggedUser?.userName !== userModel.userName"
+          @click.stop="handleClickFollow"
+          :size="ButtonSize.large"
+          :text="userModel.isFollowing ? 'Dejar seguir' : 'Seguir'"
+          class="text-nowrap"
         />
       </div>
-      <div class="flex flex-col gap-1">
-        <div class="flex flex-col">
-          <span class="font-bold">{{ userModel.demonName }}</span>
-          <span class="text-text-color-secondary font-extralight"
-            >@{{ userModel.userName }}</span
-          >
-        </div>
-        <span>{{ userModel.description }}</span>
+      <div class="lg:mr-6 mt-1 block lg:hidden">
+        <Button
+          v-if="loggedUser?.userName !== userModel.userName"
+          @click.stop="handleClickFollow"
+          :size="ButtonSize.small"
+          :text="userModel.isFollowing ? 'Dejar seguir' : 'Seguir'"
+          class="text-nowrap"
+        />
       </div>
     </div>
-    <div class="lg:mr-6 mt-1 hidden lg:block">
-      <Button
-        v-if="loggedUser?.userName !== userModel.userName"
-        @click.stop="handleClickFollow"
-        :size="ButtonSize.large"
-        :text="userModel.isFollowing ? 'Dejar seguir' : 'Seguir'"
-        class="text-nowrap"
-      />
-    </div>
-    <div class="lg:mr-6 mt-1 block lg:hidden">
-      <Button
-        v-if="loggedUser?.userName !== userModel.userName"
-        @click.stop="handleClickFollow"
-        :size="ButtonSize.small"
-        :text="userModel.isFollowing ? 'Dejar seguir' : 'Seguir'"
-        class="text-nowrap"
-      />
-    </div>
+    <span class="pl-[60px]">{{ userModel.description }}</span>
   </div>
 </template>
